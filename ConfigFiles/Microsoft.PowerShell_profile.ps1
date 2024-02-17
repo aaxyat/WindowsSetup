b9cc04451f05266687d8699fb898f59a3a126f0c
Ayush Bhattarai <mail@ayushb.com>
2024-02-17 22:22:21 +0545

    add some functionality

--- ConfigFiles/Microsoft.PowerShell_profile.ps1
+++ ConfigFiles/Microsoft.PowerShell_profile.ps1
@@ -7,87 +7,77 @@ Set-PSReadLineOption -EditMode Windows
 Invoke-Expression (&starship init powershell)
 
 function acm {
-    param(
-      [Parameter(ValueFromRemainingArguments = $true)]
-      [String[]] $message
-    )
-    git add .
-    git commit -a -m "$message"
-    git push
-  }
+        param(
+                [Parameter(ValueFromRemainingArguments = $true)]
+                [String[]] $message
+        )
+        git add .
+        git commit -a -m "$message"
+        git push
+}
 
 #choco install Script
 Function cinst {
-	sudo choco install $args
-	}
+        sudo choco install $args
+}
 
 # If so and the current host is a command line, then change to red color 
 # as warning to user that they are operating in an elevated context
 # Useful shortcuts for traversing directories
-function cd...  { cd ..\.. }
-function cd.... { cd ..\..\.. }
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
 
 # Compute file hashes - useful for checking successful downloads 
-function md5    { Get-FileHash -Algorithm MD5 $args }
-function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
 
 # Quick shortcut to start notepad
-function n      { notepad $args }
+function n { notepad $args }
 
 # Drive shortcuts
-function HKLM:  { Set-Location HKLM: }
-function HKCU:  { Set-Location HKCU: }
-function Env:   { Set-Location Env: }
+function HKLM: { Set-Location HKLM: }
+function HKCU: { Set-Location HKCU: }
+function Env: { Set-Location Env: }
 
 # Creates drive shortcut for Work Folders, if current user account is using it
-if (Test-Path "$env:USERPROFILE\Work Folders")
-{
-    New-PSDrive -Name Work -PSProvider FileSystem -Root "$env:USERPROFILE\Work Folders" -Description "Work Folders"
-    function Work: { Set-Location Work: }
+if (Test-Path "$env:USERPROFILE\Work Folders") {
+        New-PSDrive -Name Work -PSProvider FileSystem -Root "$env:USERPROFILE\Work Folders" -Description "Work Folders"
+        function Work: { Set-Location Work: }
 }
 
 
 # Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
-function dirs
-{
-    if ($args.Count -gt 0)
-    {
-        Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
-    }
-    else
-    {
-        Get-ChildItem -Recurse | Foreach-Object FullName
-    }
+function dirs {
+        if ($args.Count -gt 0) {
+                Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
+        }
+        else {
+                Get-ChildItem -Recurse | Foreach-Object FullName
+        }
 }
 
 # Simple function to start a new elevated process. If arguments are supplied then 
 # a single command is started with admin rights; if not then a new admin instance
 # of PowerShell is started.
-function admin
-{
-    if ($args.Count -gt 0)
-    {   
-       $argList = "& '" + $args + "'"
-       Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
-    }
-    else
-    {
-       Start-Process "$psHome\powershell.exe" -Verb runAs
-    }
+function admin {
+        if ($args.Count -gt 0) {   
+                $argList = "& '" + $args + "'"
+                Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+        }
+        else {
+                Start-Process "$psHome\powershell.exe" -Verb runAs
+        }
 }
 
 # Make it easy to edit this profile once it's installed
-function Edit-Profile
-{
-    if ($host.Name -match "ise")
-    {
-        $psISE.CurrentPowerShellTab.Files.Add($profile.CurrentUserAllHosts)
-    }
-    else
-    {
-        notepad $profile.CurrentUserAllHosts
-    }
+function Edit-Profile {
+        if ($host.Name -match "ise") {
+                $psISE.CurrentPowerShellTab.Files.Add($profile.CurrentUserAllHosts)
+        }
+        else {
+                notepad $profile.CurrentUserAllHosts
+        }
 }
 
 #
@@ -96,23 +86,22 @@ function Edit-Profile
 New-Alias vim nvim
 function ll { Get-ChildItem -Path $pwd -File }
 function g { cd $HOME\Documents\Github }
-function gcom
-{
-	git add .
-	git commit -m "$args"
+function gcom {
+        git add .
+        git commit -m "$args"
 }
-function lazyg
-{
-	git add .
-	git commit -m "$args"
-	git push
+function lazyg {
+        git add .
+        git commit -m "$args"
+        git push
 }
 Function Get-PubIP {
  (Invoke-WebRequest http://ifconfig.me/ip ).Content
 }
 function uptime {
-        Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';
-        EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+        Get-WmiObject win32_operatingsystem | select csname, @{LABEL = 'LastBootUpTime';
+                EXPRESSION                                           = { $_.ConverttoDateTime($_.lastbootuptime) }
+        }
 }
 function reload-profile {
         & $profile
@@ -125,7 +114,7 @@ function find-file($name) {
 }
 function unzip ($file) {
         echo("Extracting", $file, "to", $pwd)
-	$fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object{$_.FullName}
+        $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
         Expand-Archive -Path $fullFile -DestinationPath $pwd
 }
 function grep($regex, $dir) {
@@ -141,7 +130,7 @@ function touch($file) {
 function df {
         get-volume
 }
-function sed($file, $find, $replace){
+function sed($file, $find, $replace) {
         (Get-Content $file).replace("$find", $replace) | Set-Content $file
 }
 function which($name) {
@@ -164,5 +153,5 @@ function pgrep($name) {
 # See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+        Import-Module "$ChocolateyProfile"
 }
--- script.ps1
+++ script.ps1
@@ -1,4 +1,25 @@
-# script.ps1
+# Check if the script is running under administrator privileges
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-# Write output to the console
-Write-Host "Hello, World!"
+# If not running as admin, re-run the script with elevated privileges
+if (-not $isAdmin) {
+   Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $($MyInvocation.MyCommand.Path)"
+   Exit
+}
+
+
+
+# Set the global execution policy to unrestricted
+Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+
+
+# Check if Chocolatey is installed
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+   # Install Chocolatey
+   Set-ExecutionPolicy Bypass -Scope Process -Force
+   Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+   Write-Host "Chocolatey is installed."
+}
+else {
+   Write-Host "Chocolatey is already installed."
+}
