bdcdf48f8ef308f47a129a67f17964db5cfb6123
Ayush Bhattarai <mail@ayushb.com>
2024-02-18 01:13:11 +0545

    add some functionality

--- script.ps1
+++ script.ps1
@@ -39,19 +39,66 @@ else {
 
 # Install PowerShellGet and PSReadLine
 Write-Host "Installing PowerShellGet and PSReadLine..."
-Install-Module -Name PowerShellGet -Force -AllowClobber -Scope AllUsers -Confirm:$false -Force
-Install-Module -Name PSReadLine -Force -AllowClobber -Scope AllUsers -Confirm:$false -Force
+Install-Module -Name PowerShellGet -Force -AllowClobber -Scope AllUsers -Confirm:$false 
+Install-Module -Name PSReadLine -Force -AllowClobber -Scope AllUsers -Confirm:$false 
 Write-Host "PowerShellGet and PSReadLine are installed."
 
-# Install the required packages using Chocolatey
-Write-Host "Installing packages using Chocolatey..."
-choco install -y python autohotkey vscode windirstat winfsp nssm brave termius steam notepadplusplus.install gsudo git starship 7zip discord vlc mpv teracopy qbittorrent rclone yt-dlp k-litecodecpackfull revo-uninstaller adb firacode autohotkey nodejs.install curl stremio
-Write-Host "Packages installation completed."
+# # Install the required packages using Chocolatey
+# Write-Host "Installing packages using Chocolatey..."
+# choco install -y python autohotkey vscode windirstat winfsp nssm brave termius steam notepadplusplus.install gsudo git starship 7zip discord vlc mpv teracopy qbittorrent rclone yt-dlp k-litecodecpackfull revo-uninstaller adb firacode autohotkey nodejs.install curl stremio
+# Write-Host "Packages installation completed."
 
-# Install the required packages using winget
-Write-Host "Installing packages using winget..."
-winget install --id TonecInc.InternetDownloadManager
-winget install --id StardockSoftware.Start10
-winget install --id AppWorkGmbH.JDownloader2
-winget install --id HeroicGamesLauncher.HeroicGamesLauncher
-Write-Host "Packages installation completed."
+# # Install the required packages using winget
+# Write-Host "Installing packages using winget..."
+# winget install --id TonecInc.InternetDownloadManager
+# winget install --id StardockSoftware.Start10
+# winget install --id AppWorkGmbH.JDownloader2
+# winget install --id HeroicGamesLauncher.HeroicGamesLauncher
+# Write-Host "Packages installation completed."
+
+# Install the required packages from the Windows Store
+# Write-Host "Installing packages from the Windows Store..."
+# winget install --id HewlettPackard.HPCommandCenter
+# winget install --id Microsoft.BatteryPercentageIcon
+# winget install --id Microsoft.WinDynamicDesktop
+# winget install --id WhatsAppWhatsApp
+# winget install --id Viber.Viber
+# winget install --id Unigram.Unigram
+# Write-Host "Packages installation completed."
+
+
+# Download the PowerShell profile file
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/aaxyat/WinndowsSetup/main/ConfigFiles/Microsoft.PowerShell_profile.ps1" -OutFile $PROFILE
+
+# Install the profile into PowerShell 7 profile
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+   $profilePath = $PROFILE | Split-Path
+   $profilePath7 = Join-Path $profilePath "Microsoft.PowerShell_profile.ps1"
+   Copy-Item -Path $PROFILE -Destination $profilePath7 -Force
+   Write-Host "PowerShell 7 profile installed."
+}
+else {
+   Write-Host "PowerShell 7 is not installed. Cannot install the profile."
+}
+
+# Download the Windows Terminal settings file
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/aaxyat/WinndowsSetup/main/ConfigFiles/settings.json" -OutFile "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+Write-Host "Windows Terminal settings file downloaded and installed."
+
+
+# Download the shortcut.exe file
+Invoke-WebRequest -Uri "https://github.com/aaxyat/WinndowsSetup/raw/main/ConfigFiles/shortcuts.exe" -OutFile "$env:TEMP\shortcut.exe"
+
+# Copy the shortcut.exe file to shell:startup
+$shellStartup = [Environment]::GetFolderPath("Startup")
+Copy-Item -Path "$env:TEMP\shortcut.exe" -Destination $shellStartup -Force
+
+Write-Host "shortcut.exe file copied to shell:startup."
+
+# Set the default explorer open folder to "This PC"
+$explorerKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+Set-ItemProperty -Path $explorerKeyPath -Name "LaunchTo" -Value 1
+
+# Disable "Hide extensions for known file types"
+$hideExtensionsKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+Set-ItemProperty -Path $hideExtensionsKeyPath -Name "HideFileExt" -Value 0
